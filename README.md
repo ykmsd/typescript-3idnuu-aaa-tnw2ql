669174295b9a25feebbfe89668bd79e79c281f23
ykmsd <yuka1110m@gmail.com>
2022-10-24 14:19:17 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-3idnuu-aaa-tnw2ql
+
+[Edit in Codeflow ⚡️](https://jake.stackblitz.com/~/github.com/ykmsd/typescript-3idnuu-aaa-tnw2ql)
---
+++ index.ts
@@ -0,0 +1,8 @@
+// Import stylesheets
+import './style.css';
+
+// Write TypeScript code!
+const appDiv: HTMLElement = document.getElementById('app');
+appDiv.innerHTML = `<h1>TypeScripaaat ss</h1>`;
+
+console.log('test');
